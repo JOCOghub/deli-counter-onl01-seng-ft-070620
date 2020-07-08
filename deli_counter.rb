a12a74e1098b8c33855894ff140ae8eebf0a0c56
Joseph cohen <jojocohen57@gmail.com>
2020-07-08 01:05:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,34 +1,29 @@
 def line(cl)
   if cl.length == 0
     puts "The line is currently empty."
-  end 
- # phrase = "The line is currently:"
-   if cl.length > 0
-    new_str = "The line is currently: "
-    i = 0  
-      while i < cl.length
-        new_str += "#{i + 1}. #{cl[i]}"
-        i += 1
-      end
-    puts new_str    
+  elsif cl.length > 0
+    new_arr = cl.map.with_index {|name, i| name = "#{i + 1}. #{name}"}
+    puts "The line is currently: " + new_arr.join(' ')
    end
 end  
 
-def take_a_number(line, newName) 
-  line.push(newName);
-  if line.length == 1 
-    return "Welcome, #{newName}. You are number 1 in line."
-  elsif line.length > 1 
-       #revise 
-    return "Welcome, #{newName}. You are number #{idx + 1} in line.";
+def take_a_number(katz_deli, name) 
+    if katz_deli.length == 0 
+      katz_deli.push(name)
+      puts "Welcome, #{name}. You are number 1 in line."
+    elsif katz_deli.length > 0 
+      katz_deli.push(name)
+      idx = katz_deli.index(name)
+      puts "Welcome, #{name}. You are number #{idx + 1} in line.";
   end  
 end  
-def now_serving(katzDeliLine) 
-  if katzDeliLine.length == 0
-    return "There is nobody waiting to be served!";
+
+def now_serving(line) 
+  if line.length == 0
+    puts "There is nobody waiting to be served!";
   else 
-    serve = "Currently serving #{katzDeliLine[0]}."
-    shifted = katzDeliLine.shift
-    return serve
+    serve = "Currently serving #{line[0]}."
+    shifted = line.shift
+    puts serve
   end 
 end  
